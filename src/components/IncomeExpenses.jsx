b80629a4340b133acb4f8e2902c4633867de75a2
Tomas Martinez <martineztomi93@gmail.com>
2023-07-08 10:36:54 -0300

    Eliminamos logs

--- src/components/IncomeExpenses.jsx
+++ src/components/IncomeExpenses.jsx
@@ -6,7 +6,6 @@ export const IncomeExpenses = () => {
     const { transactions } = useGlobalState();
 
     const amounts = transactions.map( transaction => transaction.amount)
-    // console.log(amounts)
 
     const income = amounts.filter( item => item > 0).reduce((acc,item) => (acc += item),0).toFixed(2);
     const expense = amounts.filter( item => item < 0).reduce((acc,item) => (acc += item),0).toFixed(2) *-1;
